9ce92a59ed9593ccb86891233a556dce7ece9905
Max <hi.max@foxmail.com>
2024-12-06 23:45:56 +0800

    refactor: streamline exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-import JSBI from 'jsbi'
-export { JSBI }
-
-export {
-  BigintIsh,
-  ChainId,
-  TradeType,
-  Rounding,
-  FACTORY_ADDRESS,
-  INIT_CODE_HASH,
-  MINIMUM_LIQUIDITY
-} from './constants'
-
+export { default as JSBI } from 'jsbi'
+export * from './constants'
 export * from './errors'
 export * from './entities'
 export * from './router'
